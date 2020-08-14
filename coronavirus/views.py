0f44ae9a82090b1aaea92452d954709c2fce6fa1
jalbertoornelass <albertoornelass44@gmail.com>
2020-08-13 22:07:00 -0700

    adding table covid coutries

--- coronavirus/views.py
+++ coronavirus/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from covid import Covid
 # Create your views here.
 def home(request):
-    context = {}
-    return render(request, 'home.html')
+    covid = Covid()
+    country = covid.get_data()
+    
+    context = {'countries':country, 'top_ten':country[0:10]}
+    return render(request, 'home.html', context)
+
+         
+
