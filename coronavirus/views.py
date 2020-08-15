3d3042777ca2746c3bac6b5e5f70512aea8161ed
jalbertoornelass <albertoornelass44@gmail.com>
2020-08-14 20:29:40 -0700

    generating data and login

--- coronavirus/views.py
+++ coronavirus/views.py
@@ -1,12 +1,54 @@
 from django.shortcuts import render
 from covid import Covid
-# Create your views here.
+from django.core.paginator import Paginator
+from django.contrib.auth.models import User
+
+import pygal
+
 def home(request):
+    # Get covid data
     covid = Covid()
     country = covid.get_data()
-    
-    context = {'countries':country, 'top_ten':country[0:10]}
+
+    # pagination
+    page = request.GET.get('page', 1)
+    paginator = Paginator(country, 20)
+
+    try:
+    	countries = paginator.page(page)
+    except PageNotAnInteger:
+    	countries = paginator.page(1)
+    except EmptyPage:
+    	countries = paginator.page(paginator.num_pages)
+
+    genereting_graphic(country)
+    # pass data to dict
+    context = {'countries':countries, 'top_ten':country[0:10]}
     return render(request, 'home.html', context)
+
+
+def genereting_graphic(country):
+	hist = pygal.Bar()
+
+	hist.title = "Top ten of cases in the world"
+	#hist.xlabels = [c['country'] for c in country[0:10]]
+	
+	for c in country[0:10]:
+		hist.add(c['country'], c['confirmed'])
+
+	hist.render_to_file('static/svg/top_ten.svg')
+
+def login(request):
+	username = request.POST['username']
+	password = request.POST['password']
+	user = authenticate(request, username=username, password=password)
+	if user is not None:
+		login(request, user)
+	
+
+
+
+
 
          
 
