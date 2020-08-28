86c91a4fc266d498d15c20c91459194c33f93acf
jalbertoornelass <albertoornelass44@gmail.com>
2020-08-27 22:11:19 -0700

    country view

--- coronavirus/urls.py
+++ coronavirus/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('country_view/', views.ViewCountryData.as_view(), name="country_view"),
+	path('country_view/<int:id>/', views.country_view, name="country_view"),
 	path('', views.home, name="home"),
 ]
--- coronavirus/views.py
+++ coronavirus/views.py
@@ -4,6 +4,7 @@ from django.core.paginator import Paginator
 from django.contrib.auth.models import User
 from django.views.generic import DetailView
 import pygal
+from pygal.style import Style
 
 covid = Covid()
 country = covid.get_data()
@@ -22,24 +23,38 @@ def home(request):
 
     genereting_graphic(country)
     # pass data to dict
-    context = {'countries':countries, 'top_ten':country[0:10]}
+    context = {'countries':countries, 'top_five':country[0:5]}
     return render(request, 'home.html', context)
 
-
 def genereting_graphic(country):
-	hist = pygal.Bar(print_values=True, print_values_position='top')
-	hist.title = "Top ten of cases in the world"
+	hist = pygal.Bar(print_values=True, value_formatter=lambda x: '{}'.format(x))
+	hist.title = "Top five of cases in the world"
 	
-	for c in country[0:10]:
+	for c in country[0:5]:
 		hist.add(c['country'], c['confirmed'])
 	
-	hist.render_to_file('my-svgs/top_ten.svg')
+	hist.render_to_file('my-svgs/top_five.svg')
 	hist.render()
 
+def country_view(request, id):
+    country = covid.get_status_by_country_id(id)
+    custom_style = Style(
+        colors=('yellow', 'red', 'green', 'black'))
 
-class ViewCountryData(DetailView):
-    model = country
-    template = 'country.html'
+    hist = pygal.Bar(print_values=True, value_formatter=lambda x: '{}'.format(x), 
+        style=custom_style)
+    hist.title = 'Information about ' + country['country']
+
+    hist.add('Confirmed', country['confirmed'])
+    hist.add('Active', country['active'])
+    hist.add('Recovered', country['recovered'])
+    hist.add('Deaths', country['deaths'])
+    
+    hist.render_to_file('my-svgs/c.svg')
+    hist.render()
+    
+    return render(request, 'country.html')
+
 
     
 
